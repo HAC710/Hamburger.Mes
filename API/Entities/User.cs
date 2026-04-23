418dfb68bb6f2eb1b5c0158f9e3d7b6cf2981ceb
Nguyễn Xuân Hoàng <81058464+HAC710@users.noreply.github.com>
2026-04-23 13:47:21 +0700

    Initial project

---
+++ API/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities;
+using Microsoft.AspNetCore.Identity;
+public class User: IdentityUser
+{
+    // Thêm các trường bạn cần cho hệ thống MES
+    public string FullName { get; set; }
+    public string StaffCode { get; set; } // Mã nhân viên
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsActive { get; set; } = true;
+}
---
+++ API/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1. Khai báo DbContext
+
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 2. Thiết lập Identity
+builder.Services.AddIdentity<User, IdentityRole>(options => {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 6; // Cho phép pass đơn giản lúc dev
+        options.Password.RequireNonAlphanumeric = false;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+var app = builder.Build();
+// ... sau đó bên dưới app.Build()
+app.UseAuthentication(); // Phải đứng trước Authorization
+app.UseAuthorization();
